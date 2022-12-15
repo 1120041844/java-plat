3c300d59ac0e5cb9ffed4621c94f200c8b2788ba
xjn <1120041844@qq.com>
2022-12-15 19:57:11 +0800

    v3

--- src/main/java/com/work/plat/entity/Article.java
+++ src/main/java/com/work/plat/entity/Article.java
@@ -17,7 +17,7 @@ public class Article implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId(value = "id")
+    @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
 
     @ApiModelProperty("标题")
--- src/main/java/com/work/plat/entity/Course.java
+++ src/main/java/com/work/plat/entity/Course.java
@@ -16,7 +16,7 @@ public class Course implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("id")
-    @TableId(value = "id")
+    @TableId(value = "id",type= IdType.AUTO)
     private Integer id;
 
     @ApiModelProperty("课程名称")
