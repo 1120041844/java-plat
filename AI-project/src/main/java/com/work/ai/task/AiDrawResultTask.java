7f0db62d793f225c040566017dcb633d1dbcfeab
bu1ubiu <807960540@qq.com>
2024-12-07 18:22:37 +0800

    增加额度

--- AI-project/src/main/java/com/work/ai/task/AiDrawResultTask.java
+++ AI-project/src/main/java/com/work/ai/task/AiDrawResultTask.java
@@ -3,9 +3,11 @@ package com.work.ai.task;
 import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.work.ai.entity.bo.AiDrawDO;
+import com.work.ai.entity.bo.SysUserRemainingDO;
 import com.work.ai.enums.ApiEnum;
 import com.work.ai.enums.StatusEnum;
 import com.work.ai.mapper.AiDrawMapper;
+import com.work.ai.mapper.SysUserRemainingMapper;
 import com.work.ai.service.IAIDrawService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,8 @@ public class AiDrawResultTask {
     AiDrawMapper aiDrawMapper;
     @Autowired
     IAIDrawService iaiDrawService;
+    @Autowired
+    SysUserRemainingMapper sysUserRemainingMapper;
 
     @Scheduled(cron ="*/2 * * * * ?")
     public void selectDrawResult() {
@@ -34,6 +38,24 @@ public class AiDrawResultTask {
         }
         for (AiDrawDO aiDrawDO : aiDrawDOS) {
             iaiDrawService.getTaskResult(aiDrawDO);
+        }
+    }
+
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void increaseCreditLimit() {
+        List<SysUserRemainingDO> sysUserRemainingDOS = sysUserRemainingMapper.selectList(new LambdaQueryWrapper<>());
+        if (CollUtil.isEmpty(sysUserRemainingDOS)) {
+            return;
+        }
+        for (SysUserRemainingDO remainingDO : sysUserRemainingDOS) {
+            Long number = remainingDO.getNumber();
+            if (number < 50) {
+                Long updatedNumber = Math.min(number + 10, 50L);
+                if (!updatedNumber.equals(number)) {
+                    remainingDO.setNumber(updatedNumber);
+                    sysUserRemainingMapper.updateById(remainingDO);
+                }
+            }
         }
     }
 }
