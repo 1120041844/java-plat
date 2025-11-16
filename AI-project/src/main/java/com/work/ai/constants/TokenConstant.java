0d3939604646e4641bc3eb9992f7da214662cc1b
1120041844 <58925140+1120041844@users.noreply.github.com>
2025-11-16 22:08:18 +0800

    1

--- AI-project/src/main/java/com/work/ai/constants/TokenConstant.java
+++ AI-project/src/main/java/com/work/ai/constants/TokenConstant.java
@@ -2,7 +2,7 @@ package com.work.ai.constants;
 
 public interface TokenConstant {
 
-	String SIGN_KEY = "ydCdpH9JTsevVQgzvyArlQOdsVLoJ6dYVfX9HkjvcdTlCVWwFJ9WtEzkJbuRtA2R";
+	String SIGN_KEY = "";
 	String HEADER = "token";
 
 	Integer TOKEN_COOKIE_AGE=365*24*3600;
